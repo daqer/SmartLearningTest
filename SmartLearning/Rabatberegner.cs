50558a5f2954d1b7adfe7359bd6870cb1ed8bdc7
daqer <david_christoffersen@live.dk>
2022-09-08 19:25:31 +0200

    Cleanup

---
+++ SmartLearning/Rabatberegner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uge1Rabat
+{
+    public class Rabatberegner
+    {
+
+        public double GetPrisEfterRabat(int antalVarer, double stykPris)
+        {
+            double bruttoPris = antalVarer * stykPris;
+            double rabatprocent = 1;
+            if (bruttoPris > 1000)
+            {
+                // Indkøb over 1000 kr. giver 3% rabat
+                rabatprocent -= 0.03;
+            }
+            if (antalVarer > 10)
+            {
+                // Flere end 10 varer giver 2% rabat
+                rabatprocent -= 0.02;
+            }
+            bruttoPris *= rabatprocent;
+            return bruttoPris;
+        }
+    }
+}
