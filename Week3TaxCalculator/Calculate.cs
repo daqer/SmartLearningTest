ddb1990c14f46011ff05497dbf1d1e2b880a162e
daqer <david_christoffersen@live.dk>
2022-10-04 18:10:07 +0200

    Week 3 done

---
+++ Week3TaxCalculator/Calculate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3TaxCalculator
+{
+    public class Calculate
+    {
+        public double TaxAtChristasPresent(double christmasGift, double secondGift)
+        {
+            var tax = 0.0;
+            if (christmasGift + secondGift > 1200)
+            {
+                tax = (christmasGift > 900) ? christmasGift + secondGift : secondGift;
+            }
+            return tax;
+        }
+    }
+}
---
+++ Week3TaxCalculatorTests/TaxCalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using Week3TaxCalculator;
+using Xunit;
+
+namespace Week3TaxCalculatorTests
+{
+    public class TaxCalculatorTests
+    {
+
+        [Theory]
+        [InlineData(0, 900, 0)]
+        [InlineData(0, 1200, 0)]
+        [InlineData(0, 900, 300)]
+        [InlineData(1210, 910, 300)]
+        [InlineData(500, 900, 500)]
+        public void CalculatePresentTax(double expectedPrice, double christmasPresentPrice, double otherGiftPrice)
+        {
+            Calculate calculate = new Calculate();
+
+            double tax = calculate.TaxAtChristasPresent(christmasPresentPrice, otherGiftPrice);
+
+            Assert.Equal(expectedPrice, tax);
+        }
+    }
+}
